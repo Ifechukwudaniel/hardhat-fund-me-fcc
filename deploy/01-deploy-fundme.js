63a3bfe14a10ee0acf130cdfab4a300cfe660bfb
Angry Coding <mailidanielifechukwu@gmail.com>
2022-09-01 12:29:04 +0100

    deploy script

---
+++ deploy/01-deploy-fundme.js
@@ -0,0 +1,35 @@
+const { network } = require("hardhat")
+const { networkConfig, developmentChain } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    //when going for localhost of hardhat network we want to use a mock
+
+    let ethUsdPriceFeedAddress
+    if (developmentChain.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    let args = [ethUsdPriceFeedAddress]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+    if (
+        !developmentChain.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+    log("-----------------------------------------------------")
+}
+
+module.exports.tags = ["all", "fundme"]
